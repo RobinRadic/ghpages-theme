8956152eee39137d14d3fe2bebbc8e7d0a873def
robinradic <robin@radic.nl>
2015-01-25 20:37:12 +0100

    before remove ignore files

--- _site/assets/js/bundle/index.js
+++
@@ -1,8 +0,0 @@
-var moment = require('moment'),
-    $ = require('jquery'),
-    _ = require('lodash');
-
-module.exports = function(){
-    $('body').addClass('testbrowserify');
-    moment(new Date());
-};
